94564772c9c4d789ea99bec7ac3c4c72b70fb94f
KuroHere <pigkuro2005@gmail.com>
2026-09-04 15:14:10 +0700

    feat: hello world!

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-        'name' => config('app.name'),
-        'message' => 'Marketplace API is running',
-        'api' => url('/api/v1'),
-    ]);
+    return view('hello');
 });
